d1e010295562fbabf1d8234a1534d851967f757c
avenje <jeremyavenarius@gmail.com>
2018-03-18 14:27:32 -0600

    updated viewports with JS

--- js/init.js
+++ js/init.js
@@ -1,24 +1,25 @@
+// Parallax function for Contact Me map
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
   }); // end of document ready
 })(jQuery); // end of jQuery name space
 
-
+// Materialize Carousel function
 $(document).ready(function(){
   $('.carousel').carousel();
 });
 
+// Display current year in footer automatically
 function getYear() {
   let today= new Date();
   let currentYear= today.getFullYear();
   $("#currentYear").text(currentYear + " | Jeremy Avenarius");
 };
 getYear();
- 
+
+// Last modified stamp in footer
 function updatedStamp() {
   let lastUpdate = document.lastModified;
   const length = 10;
@@ -26,3 +27,14 @@ function updatedStamp() {
   $("#updatedStamp").text("Last updated on " + stamp);
 }
 updatedStamp();
+
+// Shorten my name for smaller screen widths
+function shortenName() {
+  if (window.innerWidth < 500) {
+    $("#myName").text("Jeremy A.");
+    console.log(window.innerWidth);
+  } else {
+    $("#myName").text("Jeremy Avenarius");
+  }
+};
+window.addEventListener("resize", shortenName());
